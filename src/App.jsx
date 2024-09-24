928853edf71427f30f45cb7fc3429ea1264234d8
Levina Tani <edelaida@gmail.com>
2024-09-24 16:08:56 +0300

    httpp-eee

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React levina 555 666 7878</h1>
+
+      <p>Click to learn more</p>
+    </>
+  );
+}
+
+export default App;
